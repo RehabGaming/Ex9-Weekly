e4a7a387d5291d3ba494be2a749ba14ba17be0c5
Ofekmarom <ofekmarom9@gmail.com>
2025-01-15 13:29:16 +0200

    Update BallSpawner.cs

--- Assets/Scripts/Erel_Scripts/BallSpawner.cs
+++ Assets/Scripts/Erel_Scripts/BallSpawner.cs
@@ -1,20 +1,33 @@
 using Fusion;
 using UnityEngine;
 
-public class BallSpawner : NetworkBehaviour {
-    [Networked] private TickTimer spawnTimer { get; set; }
+public class BallSpawner : NetworkBehaviour
+{
+    [Networked]
+    private TickTimer spawnTimer { get; set; }
 
-    [SerializeField] float timeBetweenSpawns = 1f;
-    [SerializeField] NetworkObject prefabToSpawn;
+    [SerializeField]
+    private float timeBetweenSpawns = 1f;
 
-    public override void Spawned() {
+    [SerializeField]
+    private NetworkObject prefabToSpawn;
+
+    public override void Spawned()
+    {
+        // Initialization logic if needed
     }
 
-    public override void FixedUpdateNetwork() {
-        if (spawnTimer.ExpiredOrNotRunning(Runner)) {
-            Runner.Spawn(prefabToSpawn,
-                transform.position, transform.rotation,
-                Object.InputAuthority);
+    public override void FixedUpdateNetwork()
+    {
+        if (spawnTimer.ExpiredOrNotRunning(Runner))
+        {
+            Runner.Spawn(
+                prefabToSpawn,
+                transform.position,
+                transform.rotation,
+                Object.InputAuthority
+            );
+
             spawnTimer = TickTimer.CreateFromSeconds(Runner, timeBetweenSpawns);
         }
     }
